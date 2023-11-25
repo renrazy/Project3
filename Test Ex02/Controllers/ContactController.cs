ee462e84d572431c9f0eb6cc514f69c78826a800
renrazy <vsa3559@gmail.com>
2023-11-25 16:17:57 +0700

    Hello

---
+++ Test Ex02/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test_Ex02.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Detail()
+        {
+            return View("Details");
+        }
+        public ActionResult Mycontent()
+        {
+            return View("Hello");
+        }
+        public int add()
+    }
+}
---
+++ Test Ex02/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test_Ex02.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.name = "Show your home page";
+            return View();
+        }
+        //Defferent name
+        public ActionResult showInfo()
+        {
+            return View("show");  
+        }
+        //In share Folder
+        public ActionResult Detail()
+        {
+            return View("Detail");
+        }
+        //Deferrent Controller
+        public ActionResult ShowmoreDetail()
+        {
+            return View("~/Views/Contact/Myprofile.cshtml");
+        }
+    }
+}
